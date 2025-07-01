63d242895e7a0c85ffdbea59f323b98f82fdee54
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-07-01 10:52:29 +0200

    tests(omnievent): log decode & listener decoding test

--- omnievent/src/event_manager.rs
+++ omnievent/src/event_manager.rs
@@ -4,3 +4,50 @@
 pub mod db;
 pub(crate) mod listener;
 
+#[cfg(test)]
+mod tests {
+    pub(crate) mod test_contracts {
+        use crate::event_manager::tests::test_contracts::EventEmitter::EventEmitterInstance;
+        use crate::proto_types::BlockSafety;
+        use crate::types::{EventStreamId, ParsedEventField, RegisteredEvent};
+        use alloy::dyn_abi::DynSolType;
+        use alloy::network::Network;
+        use alloy::providers::Provider;
+
+        alloy::sol! {
+            #[sol(rpc, bytecode = "6080604052348015600e575f5ffd5b506101ec8061001c5f395ff3fe608060405234801561000f575f5ffd5b5060043610610029575f3560e01c80632536f1271461002d575b5f5ffd5b610047600480360381019061004291906100ef565b610049565b005b7f500918a1acf84fe22df8e73c039449df2f37619cf220d2a4d382cddec5e088e1828260405161007a929190610194565b60405180910390a15050565b5f5ffd5b5f5ffd5b5f5ffd5b5f5ffd5b5f5ffd5b5f5f83601f8401126100af576100ae61008e565b5b8235905067ffffffffffffffff8111156100cc576100cb610092565b5b6020830191508360018202830111156100e8576100e7610096565b5b9250929050565b5f5f6020838503121561010557610104610086565b5b5f83013567ffffffffffffffff8111156101225761012161008a565b5b61012e8582860161009a565b92509250509250929050565b5f82825260208201905092915050565b828183375f83830152505050565b5f601f19601f8301169050919050565b5f610173838561013a565b935061018083858461014a565b61018983610158565b840190509392505050565b5f6020820190508181035f8301526101ad818486610168565b9050939250505056fea264697066735822122009bdd348f95f2e120079efff8b79e472ad4d569739061e9d398a4d57a765dd5d64736f6c634300081e0033")]
+            contract EventEmitter {
+                event StringEmitted(string value);
+                function emitString(string calldata _value) external {
+                    emit StringEmitted(_value);
+                }
+            }
+        }
+
+        pub(crate) async fn deploy_event_emitter<P, N>(provider: P) -> EventEmitterInstance<P, N>
+        where
+            P: Provider<N>,
+            N: Network,
+        {
+            EventEmitterInstance::deploy(provider).await.unwrap()
+        }
+
+        pub(crate) async fn get_string_registered_event<P, N>(
+            instance: &EventEmitterInstance<P, N>,
+        ) -> RegisteredEvent
+        where
+            P: Provider<N>,
+            N: Network,
+        {
+            RegisteredEvent::try_new(
+                EventStreamId::new(b"EventEmitterInstance::StringEmitted"),
+                instance.provider().get_chain_id().await.unwrap(),
+                *instance.address(),
+                "StringEmitted".to_owned(),
+                vec![ParsedEventField::new(DynSolType::String, false)],
+                BlockSafety::Latest,
+            )
+            .unwrap()
+        }
+    }
+}
--- omnievent/src/event_manager/listener.rs
+++ omnievent/src/event_manager/listener.rs
@@ -138,7 +138,7 @@ impl EventListener {
         }
     }
 
-    #[tracing::instrument(skip(self))]
+    #[tracing::instrument(skip_all)]
     async fn main_loop(
         self,
         sender: tokio::sync::mpsc::Sender<DecodedEvent>,
@@ -248,4 +248,148 @@ fn decode_log(
         .collect();
 
     Ok(fields)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::event_manager::tests::test_contracts;
+    use crate::proto_types::BlockSafety;
+    use crate::types::ParsedEventField;
+    use alloy::dyn_abi::{DynSolType, DynSolValue};
+    use alloy::node_bindings::Anvil;
+    use alloy::primitives::{Address, B256, U256, bytes};
+    use alloy::providers::{ProviderBuilder, WsConnect};
+    use futures_util::StreamExt;
+    use std::str::FromStr;
+    use std::time::Duration;
+
+    #[test]
+    fn can_decode_log() {
+        // Taken from https://polygonscan.com/tx/0x007688a217354bedd51a3c7bf1c54b663943748ee9e7000c555ec41e5ba1d722#eventlog#470
+        let address = Address::from_str("0x455bfe4B1B4393b458d413E2B0778A95F9B84B82").unwrap();
+
+        // RandomnessRequested (
+        //  index_topic_1 uint256 requestID = 60,
+        //  index_topic_2 uint256 nonce = 60,
+        //  index_topic_3 address requester = 0x3DD01dDFbADCE59ce2D214ce8CF9618707E03782,
+        //  uint256 requestedAt = 1747570723
+        // )
+        let request_id = U256::from(60u64);
+        let nonce = U256::from(60u64);
+        let requester = Address::from_str("0x3DD01dDFbADCE59ce2D214ce8CF9618707E03782").unwrap();
+        let requested_at = U256::from(1747570723u64);
+
+        let log = {
+            let inner_log = alloy::primitives::Log::new(
+                address,
+                vec![
+                    B256::from_str(
+                        "0xeee7195b6cee0fa7044c3af0b86fe2febb1d2703d71191f44052ba0d60ffda64",
+                    )
+                    .unwrap(),
+                    request_id.into(),
+                    nonce.into(),
+                    requester.into_word(),
+                ],
+                bytes!("0x000000000000000000000000000000000000000000000000000000006829d023"),
+            )
+            .unwrap();
+
+            let mut log = Log::default();
+            log.inner = inner_log;
+            log
+        };
+
+        let event = RegisteredEvent::try_new(
+            EventStreamId::nil(),
+            137,
+            address,
+            "RandomnessRequested".to_owned(),
+            vec![
+                ParsedEventField::new(DynSolType::Uint(256), true),
+                ParsedEventField::new(DynSolType::Uint(256), true),
+                ParsedEventField::new(DynSolType::Address, true),
+                ParsedEventField::new(DynSolType::Uint(256), false),
+            ],
+            BlockSafety::Finalized,
+        )
+        .unwrap();
+
+        let decoded = decode_log(&log, &event).unwrap();
+        assert_eq!(decoded.len(), 4);
+        assert_eq!(decoded[0].data, request_id.into());
+        assert_eq!(decoded[1].data, nonce.into());
+        assert_eq!(decoded[2].data, requester.into());
+        assert_eq!(decoded[3].data, requested_at.into());
+    }
+
+    #[tokio::test]
+    async fn listener_emits_decoded_events() {
+        let event_string = "TestString".to_owned();
+
+        let anvil = Anvil::new().spawn();
+        let wallet = anvil.wallet().expect("anvil should have a wallet");
+        let ws = WsConnect::new(anvil.ws_endpoint());
+
+        let provider = ProviderBuilder::new()
+            .with_gas_estimation()
+            .wallet(wallet)
+            .connect_ws(ws)
+            .await
+            .unwrap();
+
+        let emitter_instance = test_contracts::deploy_event_emitter(&provider).await;
+        let string_emitted_event =
+            test_contracts::get_string_registered_event(&emitter_instance).await;
+
+        let event_id = string_emitted_event.id;
+        let stream = emitter_instance
+            .StringEmitted_filter()
+            .subscribe()
+            .await
+            .unwrap()
+            .into_stream()
+            .flat_map(futures::stream::iter)
+            .map(move |(_, log)| (event_id, log))
+            .boxed();
+
+        let reg = InternalEventStreamRegistration {
+            event: string_emitted_event,
+            stream,
+        };
+
+        // Start listener and register event stream
+        let mut listener_handle = EventListener::default().run();
+        listener_handle.register_event_stream(reg).await.unwrap();
+
+        // Generate a new event
+        emitter_instance
+            .emitString(event_string.clone())
+            .send()
+            .await
+            .unwrap()
+            .watch()
+            .await
+            .unwrap();
+
+        // Get event through listener_handle
+        let decoded_event = tokio::time::timeout(
+            Duration::from_millis(1000),
+            listener_handle.event_stream().unwrap().next(),
+        )
+        .await
+        .expect("failed to get event within timeout")
+        .expect("stream closed");
+        assert_eq!(decoded_event.event_id, event_id);
+        assert_eq!(decoded_event.data.len(), 1);
+        assert_eq!(decoded_event.data[0].sol_type_str, "string");
+        assert_eq!(
+            decoded_event.data[0].data,
+            DynSolValue::String(event_string)
+        );
+        assert_eq!(decoded_event.data[0].indexed, false);
+
+        listener_handle.cancel().await.unwrap();
+    }
 }
