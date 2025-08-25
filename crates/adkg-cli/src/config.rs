9b8298c37bd1d265d6896241245a0ce9f860e240
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-08-25 18:36:47 +0200

    feat(adkg-cli/bls12-381): implement rescue for bls12-381

---
+++ crates/adkg-cli/src/config.rs
@@ -0,0 +1,43 @@
+use crate::keygen::PublicKeyMaterial;
+use libp2p::{Multiaddr, PeerId};
+use serde::{Deserialize, Serialize};
+use std::num::NonZeroUsize;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct NodeDetail {
+    pub id: NonZeroUsize,
+    #[serde(flatten)]
+    pub public_key_material: PublicKeyMaterial,
+    pub multiaddr: Multiaddr,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GroupConfig {
+    pub n: NonZeroUsize,
+    pub t: NonZeroUsize,
+    pub start_time: chrono::DateTime<chrono::Utc>,
+    pub nodes: Vec<NodeDetail>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct AdkgSecret {
+    pub adkg_scheme_name: String,
+    pub genesis_timestamp: i64,
+    pub sk: String,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
+pub struct AdkgPublic {
+    pub adkg_scheme_name: String,
+    pub genesis_timestamp: i64,
+    pub group_pk: String,
+    pub node_pks: Vec<AdkgNodePk>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AdkgNodePk {
+    pub id: NonZeroUsize,
+    pub pk: String,
+    pub peer_id: PeerId,
+    pub multiaddr: Multiaddr,
+}
