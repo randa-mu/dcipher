6d4841cde5e4673016727890a9d3457c4f02074e
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-09-18 18:20:09 +0200

    fix solc for consistency

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -9,22 +9,29 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# you might expect that forge uses the one in the forge.toml file...
+# you'd be wrong :|
+SOLC_VERSION=0.8.28
+
 # As we're generating modules, we can skip the cargo toml check..
 # You _can_ generate *ALL* the bindings for each project, but it slows down the build
 # and there are weird conflicts with the `Factory` CREATE2 deployer stuff that presently break with foundry
 # see: https://github.com/foundry-rs/foundry/issues/11705
 pushd $ROOT_DIR/randomness-solidity
-forge bind --bindings-path $ROOT_DIR/generated/src/randomness --skip-cargo-toml --module \
+forge bind --bindings-path $ROOT_DIR/generated/src/randomness \
+--skip-cargo-toml \
+--module \
+--use $SOLC_VERSION \
 --select RandomnessSender \
 --select TypesLib \
---select SignatureSender \
---overwrite
+--select SignatureSender
 popd
 
-git --no-pager diff
-
 pushd $ROOT_DIR/blocklock-solidity
-forge bind --bindings-path $ROOT_DIR/generated/src/blocklock --skip-cargo-toml --module \
+forge bind --bindings-path $ROOT_DIR/generated/src/blocklock \
+--skip-cargo-toml \
+--module \
+--use $SOLC_VERSION \
 --select BlocklockSender \
 --select TypesLib \
 --select DecryptionSender \
@@ -36,7 +43,10 @@ forge bind --bindings-path $ROOT_DIR/generated/src/blocklock --skip-cargo-toml -
 popd
 
 pushd $ROOT_DIR/onlyswaps-solidity
-forge bind --bindings-path $ROOT_DIR/generated/src/onlyswaps --skip-cargo-toml --module \
+forge bind --bindings-path $ROOT_DIR/generated/src/onlyswaps \
+--skip-cargo-toml \
+--module \
+--use $SOLC_VERSION \
 --select Router \
 --select IRouter \
 --select ERC20FaucetToken \
