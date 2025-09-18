56fe330c30e1e2de0bfd4b5d4000e6d2fd540f61
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-09-18 15:46:26 +0200

    chore: move omnievent to ./crates

--- crates/omnievent/build.rs
+++ crates/omnievent/build.rs
@@ -1,7 +1,7 @@
 fn main() -> std::io::Result<()> {
     tonic_build::configure().bytes(["."]).compile_protos(
-        &["../dcipher-proto/omnievent/events.proto"],
-        &["../dcipher-proto/omnievent/"],
+        &["../../dcipher-proto/omnievent/events.proto"],
+        &["../../dcipher-proto/omnievent/"],
     )?;
     Ok(())
 }
