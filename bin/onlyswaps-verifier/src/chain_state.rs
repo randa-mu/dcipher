53cc6195a64bd76ab3f7847148db0885582d924b
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-10-27 13:06:02 +0100

    fix(onlyswaps-verifier): clippy fix

--- bin/onlyswaps-verifier/src/chain_state.rs
+++ bin/onlyswaps-verifier/src/chain_state.rs
@@ -5,7 +5,7 @@ use alloy::network::EthereumWallet;
 use alloy::primitives::{Address, Bytes, FixedBytes};
 use alloy::providers::{DynProvider, Provider, ProviderBuilder, WsConnect};
 use alloy::signers::local::PrivateKeySigner;
-use anyhow::{Context, anyhow};
+use anyhow::anyhow;
 use config::network::NetworkConfig;
 use futures::future::{try_join, try_join_all};
 use generated::onlyswaps::router::IRouter::SwapRequestParameters;
