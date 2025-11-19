48cffcf0aa93a14d2aa2a97374c0da819207a0f8
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-11-19 10:16:54 +0100

    chore: include IHookExecutor bindings

--- generate-bindings.sh
+++ generate-bindings.sh
@@ -54,6 +54,7 @@ forge bind --bindings-path "$ROOT_DIR/crates/generated/src/onlyswaps" \
 --use $SOLC_VERSION \
 --select SwapRequestParameters \
 --select IRouter \
+--select IHookExecutor \
 --select ERC20FaucetToken \
 --select IERC20 \
 --select ErrorsLib \
