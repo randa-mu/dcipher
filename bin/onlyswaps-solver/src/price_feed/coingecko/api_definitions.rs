4b37bd29792820c6e5d7e6b7c21ff1abc20459e3
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-11-20 12:29:57 +0100

    feat(solver/price-feed): implement coingecko api definitions

---
+++ bin/onlyswaps-solver/src/price_feed/coingecko/api_definitions.rs
@@ -0,0 +1,79 @@
+use alloy::primitives::Address;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+/// An entry of the Asset platforms List endpoint <https://docs.coingecko.com/reference/asset-platforms-list>.
+/// We only include `id`, and `chain_identifier` here.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AssetPlatform {
+    pub id: String,
+    pub chain_identifier: u64,
+    pub native_coin_id: String,
+}
+
+/// The output of the Asset platforms List endpoint <https://docs.coingecko.com/reference/asset-platforms-list>.
+pub type AssetPlatforms = Vec<AssetPlatform>;
+
+/// Usd price of a token.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct TokenPrice {
+    pub usd: f64,
+}
+
+/// Output of the Coin Price by Token Addresses endpoint <https://docs.coingecko.com/reference/simple-token-price>.
+pub type TokenPriceMap = HashMap<Address, TokenPrice>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy::primitives::address;
+
+    #[test]
+    fn decode_asset_platforms() {
+        let response = r#"[
+          {
+            "id": "polygon-pos",
+            "chain_identifier": 137,
+            "name": "Polygon POS",
+            "shortname": "MATIC",
+            "native_coin_id": "matic-network",
+            "image": {
+              "thumb": "https://coin-images.coingecko.com/asset_platforms/images/15/thumb/polygon_pos.png?1706606645",
+              "small": "https://coin-images.coingecko.com/asset_platforms/images/15/small/polygon_pos.png?1706606645",
+              "large": "https://coin-images.coingecko.com/asset_platforms/images/15/large/polygon_pos.png?1706606645"
+            }
+          }
+        ]"#;
+
+        let asset_platforms: AssetPlatforms =
+            serde_json::from_str(response).expect("to decode asset platforms response");
+        assert_eq!(asset_platforms.len(), 1, "to have exactly one element");
+        assert_eq!(asset_platforms[0].id, "polygon-pos");
+        assert_eq!(asset_platforms[0].chain_identifier, 137);
+        assert_eq!(asset_platforms[0].native_coin_id, "matic-network");
+    }
+
+    #[test]
+    fn decode_coin_price_by_token_addresses() {
+        let expected_usd_price = 67187.3358936566;
+        let response = r#"{
+          "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599": {
+            "usd": 67187.3358936566,
+            "usd_market_cap": 1317802988326.25,
+            "usd_24h_vol": 31260929299.5248,
+            "usd_24h_change": 3.63727894677354,
+            "last_updated_at": 1711356300
+          }
+        }"#;
+
+        let price_map: TokenPriceMap = serde_json::from_str(response)
+            .expect("to decode coin price by token addresses response");
+        let price = price_map
+            .get(&address!("0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"))
+            .expect("to be a valid entry");
+        assert!(
+            (price.usd - expected_usd_price).abs() <= 1e-06,
+            "usd price to be approx equal"
+        );
+    }
+}
