1376c75e95235aee2215d4d3b5fe2c997e495a3c
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-11-21 16:31:49 +0100

    fix(solver): make demo api key optional

--- bin/onlyswaps-solver/src/price_feed/coingecko/builder.rs
+++ bin/onlyswaps-solver/src/price_feed/coingecko/builder.rs
@@ -14,6 +14,7 @@ pub struct CoinGeckoClientBuilder {
     api_key_header: String,
     endpoint: String,
     api_key: Option<String>,
+    api_key_required: bool,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -37,6 +38,7 @@ impl Default for CoinGeckoClientBuilder {
             endpoint: PRO_API_URL.to_owned(),
             api_key_header: PRO_API_KEY_HEADER.to_owned(),
             api_key: None,
+            api_key_required: true,
         }
     }
 }
@@ -49,6 +51,7 @@ impl CoinGeckoClientBuilder {
     pub fn use_demo_api(mut self) -> Self {
         self.endpoint = DEMO_API_URL.to_owned();
         self.api_key_header = DEMO_API_KEY_HEADER.to_owned();
+        self.api_key_required = false;
         self
     }
 
@@ -58,22 +61,27 @@ impl CoinGeckoClientBuilder {
     }
 
     pub fn build(self) -> Result<CoinGeckoClient, CoinGeckoClientBuilderError> {
-        let endpoint: Url = self.endpoint.parse()?;
-        let api_key = self
-            .api_key
-            .ok_or(CoinGeckoClientBuilderError::MissingApiKey)?;
-        let mut api_key: HeaderValue = api_key
-            .parse()
-            .map_err(|_| CoinGeckoClientBuilderError::ParseApiKey)?;
-        api_key.set_sensitive(true);
+        if self.api_key_required && self.api_key.is_none() {
+            // api_key unset while required, return Err
+            Err(CoinGeckoClientBuilderError::ParseApiKey)?
+        }
 
+        let endpoint: Url = self.endpoint.parse()?;
         let mut headers = HeaderMap::new();
         headers.insert("Content-Type", "application/json".parse().unwrap());
-        headers.insert(
-            HeaderName::from_str(&self.api_key_header)
-                .map_err(|_| CoinGeckoClientBuilderError::ParseApiKey)?,
-            api_key,
-        );
+
+        if let Some(api_key) = self.api_key {
+            let mut api_key: HeaderValue = api_key
+                .parse()
+                .map_err(|_| CoinGeckoClientBuilderError::ParseApiKey)?;
+            api_key.set_sensitive(true);
+
+            headers.insert(
+                HeaderName::from_str(&self.api_key_header)
+                    .map_err(|_| CoinGeckoClientBuilderError::ParseApiKey)?,
+                api_key,
+            );
+        }
 
         let client = CoinGeckoClient::new(endpoint, headers)?;
         Ok(client)
