ee786e91aea5bc7b3670275c38858476e592f4dd
Robin Müller <33422394+azixus@users.noreply.github.com>
2025-12-04 16:37:44 +0100

    feat(state-api): include tx hash of request, fulfillment and verification

--- bin/onlyswaps-state-api/src/state.rs
+++ bin/onlyswaps-state-api/src/state.rs
@@ -1,12 +1,15 @@
 use crate::network_bus::NetworkBus;
 use crate::omnievent::{StateType, StateUpdate, StateUpdateSource};
 use crate::serde::{LongNumber, ShortNumber};
-use alloy::primitives::{Address, FixedBytes, U256};
+use alloy::primitives::{Address, B256, FixedBytes, TxHash, U256};
 use alloy::providers::DynProvider;
 use generated::onlyswaps::i_router::IRouter::SwapRequestParametersWithHooks;
+use std::hash::HashMap;
 use serde::Serialize;
 use std::fmt::Display;
 use std::time::SystemTime;
+
+type RequestId = B256;
 
 pub(crate) struct StateMachine {
     network_bus: NetworkBus<DynProvider>,
@@ -15,12 +18,12 @@ pub(crate) struct StateMachine {
 
 #[derive(Clone, Debug, Default, Serialize)]
 pub(crate) struct AppState {
-    pub transactions: Vec<SwapTransaction>,
+    pub transactions: HashMap<RequestId, SwapTransaction>,
 }
 
 #[derive(Debug, Clone, Serialize, PartialEq)]
 pub(crate) struct SwapTransaction {
-    pub request_id: FixedBytes<32>,
+    pub request_id: RequestId,
     pub src_chain_id: ShortNumber,
     pub dest_chain_id: ShortNumber,
     pub sender: Address,
@@ -36,6 +39,9 @@ pub(crate) struct SwapTransaction {
     pub requested_time: ShortNumber,
     pub solved_time: Option<ShortNumber>,
     pub verified_time: Option<ShortNumber>,
+    pub requested_tx: TxHash,
+    pub solved_tx: Option<TxHash>,
+    pub verified_tx: Option<TxHash>,
 }
 
 impl StateMachine {
@@ -43,7 +49,7 @@ impl StateMachine {
         Self {
             network_bus,
             state: AppState {
-                transactions: Vec::new(),
+                transactions: Default::default(),
             },
         }
     }
@@ -60,6 +66,7 @@ impl StateMachine {
             chain_id,
             request_id,
             state_type,
+            tx_hash,
             #[cfg_attr(not(feature = "metrics"), allow(unused))]
             source,
         } = update;
@@ -68,7 +75,7 @@ impl StateMachine {
         // let's deal with them first
         if state_type == StateType::Fulfilled {
             return self
-                .apply_fulfilled_state(chain_id, request_id, source)
+                .apply_fulfilled_state(chain_id, request_id, tx_hash, source)
                 .await;
         }
 
@@ -80,27 +87,38 @@ impl StateMachine {
         let params = client.fetch_parameters(request_id).await?;
         match state_type {
             StateType::Requested | StateType::FeeUpdated => {
-                self.state
+                // need to preserve original tx hash
+                let maybe_tx_hash = self
+                    .state
                     .transactions
-                    .retain(|t| t.request_id != request_id);
-                self.state.transactions.push(SwapTransaction {
+                    .remove(&request_id)
+                    .map(|tx| tx.requested_tx);
+
+                self.state.transactions.insert(
                     request_id,
-                    src_chain_id: params.srcChainId.into(),
-                    dest_chain_id: params.dstChainId.into(),
-                    sender: params.sender,
-                    recipient: params.recipient,
-                    token_in: params.tokenIn,
-                    token_out: params.tokenOut,
-                    amount_in: calculate_amount_in(&params).into(),
-                    amount_out: params.amountOut.into(),
-                    solver_fee: params.solverFee.into(),
-                    verification_fee: params.verificationFee.into(),
-                    state: SwapState::Submitted.to_string(),
-                    solver: None,
-                    requested_time: params.requestedAt.into(),
-                    solved_time: None,
-                    verified_time: None,
-                });
+                    SwapTransaction {
+                        request_id,
+                        src_chain_id: params.srcChainId.into(),
+                        dest_chain_id: params.dstChainId.into(),
+                        sender: params.sender,
+                        recipient: params.recipient,
+                        token_in: params.tokenIn,
+                        token_out: params.tokenOut,
+                        amount_in: calculate_amount_in(&params).into(),
+                        amount_out: params.amountOut.into(),
+                        solver_fee: params.solverFee.into(),
+                        verification_fee: params.verificationFee.into(),
+                        state: SwapState::Submitted.to_string(),
+                        solver: None,
+                        requested_time: params.requestedAt.into(),
+                        solved_time: None,
+                        verified_time: None,
+                        // use original tx_hash in case of a FeeUpdated event
+                        requested_tx: maybe_tx_hash.unwrap_or(tx_hash),
+                        solved_tx: None,
+                        verified_tx: None,
+                    },
+                );
 
                 #[cfg(feature = "metrics")]
                 // update metrics if the event is fresh
@@ -123,36 +141,37 @@ impl StateMachine {
                 }
             }
             StateType::Verified => {
-                let (maybe_solver, maybe_solved_time) = self
+                let (maybe_solver, maybe_solved_time, maybe_requested_tx, maybe_solved_tx) = self
                     .state
                     .transactions
-                    .iter()
-                    .find(|t| t.request_id == request_id)
-                    .map(|it| (it.solver, it.solved_time.clone()))
-                    .unwrap_or((None, None));
+                    .remove(&request_id)
+                    .map(|it| (it.solver, it.solved_time, it.requested_tx, it.solved_tx))
+                    .unwrap_or_default();
 
-                self.state
-                    .transactions
-                    .retain(|t| t.request_id != request_id);
-
-                self.state.transactions.push(SwapTransaction {
+                self.state.transactions.insert(
                     request_id,
-                    src_chain_id: params.srcChainId.into(),
-                    dest_chain_id: params.dstChainId.into(),
-                    sender: params.sender,
-                    recipient: params.recipient,
-                    token_in: params.tokenIn,
-                    token_out: params.tokenOut,
-                    amount_in: calculate_amount_in(&params).into(),
-                    amount_out: params.amountOut.into(),
-                    solver_fee: params.solverFee.into(),
-                    verification_fee: params.verificationFee.into(),
-                    state: SwapState::Verified.to_string(),
-                    requested_time: params.requestedAt.into(),
-                    solved_time: maybe_solved_time,
-                    solver: maybe_solver,
-                    verified_time: Some(ShortNumber(now()?)),
-                });
+                    SwapTransaction {
+                        request_id,
+                        src_chain_id: params.srcChainId.into(),
+                        dest_chain_id: params.dstChainId.into(),
+                        sender: params.sender,
+                        recipient: params.recipient,
+                        token_in: params.tokenIn,
+                        token_out: params.tokenOut,
+                        amount_in: calculate_amount_in(&params).into(),
+                        amount_out: params.amountOut.into(),
+                        solver_fee: params.solverFee.into(),
+                        verification_fee: params.verificationFee.into(),
+                        state: SwapState::Verified.to_string(),
+                        requested_time: params.requestedAt.into(),
+                        solved_time: maybe_solved_time,
+                        solver: maybe_solver,
+                        verified_time: Some(ShortNumber(now()?)),
+                        requested_tx: maybe_requested_tx,
+                        solved_tx: maybe_solved_tx,
+                        verified_tx: Some(tx_hash),
+                    },
+                );
 
                 #[cfg(feature = "metrics")]
                 // report metrics if the event is fresh
@@ -175,6 +194,7 @@ impl StateMachine {
         &mut self,
         chain_id: u64,
         request_id: FixedBytes<32>,
+        tx_hash: TxHash,
         #[cfg_attr(not(feature = "metrics"), allow(unused))] // only used with metrics feature
         source: StateUpdateSource,
     ) -> anyhow::Result<AppState> {
@@ -193,28 +213,38 @@ impl StateMachine {
             .expect("got a chain_id for a network we don't support - this shouldn't be possible");
         let params = src_client.fetch_parameters(request_id).await?;
 
-        // then build the transaction
-        self.state
+        let (maybe_verified_time, maybe_requested_tx, maybe_verified_tx) = self
+            .state
             .transactions
-            .retain(|t| t.request_id != request_id);
-        self.state.transactions.push(SwapTransaction {
+            .remove(&request_id)
+            .map(|it| (it.verified_time, it.requested_tx, it.verified_tx))
+            .unwrap_or_default();
+
+        // then build the transaction
+        self.state.transactions.insert(
             request_id,
-            src_chain_id: params.srcChainId.into(),
-            dest_chain_id: params.dstChainId.into(),
-            sender: params.sender,
-            recipient: params.recipient,
-            token_in: params.tokenIn,
-            token_out: params.tokenOut,
-            amount_in: calculate_amount_in(&params).into(),
-            amount_out: params.amountOut.into(),
-            solver_fee: params.solverFee.into(),
-            verification_fee: params.verificationFee.into(),
-            solver: Some(receipt.solver),
-            state: SwapState::Fulfilled.to_string(),
-            requested_time: params.requestedAt.into(),
-            solved_time: Some(receipt.fulfilledAt.into()),
-            verified_time: None,
-        });
+            SwapTransaction {
+                request_id,
+                src_chain_id: params.srcChainId.into(),
+                dest_chain_id: params.dstChainId.into(),
+                sender: params.sender,
+                recipient: params.recipient,
+                token_in: params.tokenIn,
+                token_out: params.tokenOut,
+                amount_in: calculate_amount_in(&params).into(),
+                amount_out: params.amountOut.into(),
+                solver_fee: params.solverFee.into(),
+                verification_fee: params.verificationFee.into(),
+                solver: Some(receipt.solver),
+                state: SwapState::Fulfilled.to_string(),
+                requested_time: params.requestedAt.into(),
+                solved_time: Some(receipt.fulfilledAt.into()),
+                verified_time: maybe_verified_time,
+                requested_tx: maybe_requested_tx,
+                solved_tx: Some(tx_hash),
+                verified_tx: maybe_verified_tx,
+            },
+        );
 
         #[cfg(feature = "metrics")]
         // report metrics if the event is fresh
